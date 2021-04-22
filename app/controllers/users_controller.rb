c0c96aaa118f9175f502d5c445abbd3f9701ead0
Carlos Ospina <carlossospinag@gmail.com>
2021-04-22 16:19:52 -0500

    fix helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,28 @@
 class UsersController < ApplicationController
-  class UsersController < ApplicationController
-    def show
-      @user = User.find(params[:id])
+  before_action :logged_in_user, only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = 'Welcome to the app!'
+      redirect_to @user
+    else
+      render 'new'
     end
+  end
 
-    def new
-      @user = User.new
-    end
+  private
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        flash[:success] = 'Welcome to the app!'
-        redirect_to @user
-      else
-        render 'new'
-      end
-
-    private
-
-    def user_params
-      params.require(:user).permit(:username, :fullname)
-    end
+  def user_params
+    params.require(:user).permit(:username, :fullname)
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,2 @@
 module UsersHelper
-    before_action :logged_in_user, only: [:show]
 end

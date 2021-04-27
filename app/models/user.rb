6f24d15e3561695175f6d3e9671ab1d25e704da4
Carlos Ospina <carlossospinag@gmail.com>
2021-04-27 11:51:04 -0500

    update Ui

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_many :followers, through: :followerships, source: :followed
 
   validates :username, presence: true, uniqueness: true
-  validates :fullname, presence: true
+  validates :fullname, presence: true, length: { maximum: 10 }
 
   # trhows a list of users to follow
   def follow_suggest

12800f2f3e87ee8bc5cd6bc1ffe5f097ea0d8d47
Carlos Ospina <carlossospinag@gmail.com>
2021-04-26 19:26:55 -0500

    add methods to user model to fecht follow_suggest

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,20 @@ class User < ApplicationRecord
   has_many :followers, through: :followerships, source: :followed
 
   has_many :inverse_followerships, class_name: 'Following', foreign_key: :followed_id, dependent: :delete_all
-  has_many :followed_users, through: :inverse_followerships, source: :followed
+  has_many :followed_users, through: :inverse_followerships, source: :follower
 
   validates :username, presence: true, uniqueness: true
   validates :fullname, presence: true
 
-  def follow
+  scope :all_but, ->(followed) { where.not(id: followed) }
 
+  # thows a list of users to follow
+  def follow_suggest
+    User.all_but(self).map { |usr| usr unless following?(usr) }.compact
+  end
+
+  def following?(user)
+    followed_users.include?(user)
   end
 
   def follow_and_own_opinions

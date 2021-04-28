f2eaa66d1b6db1226c8d43039975548b507d8696
Carlos Ospina <carlossospinag@gmail.com>
2021-04-28 17:48:00 -0500

    update vote  create method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,6 @@ class VotesController < ApplicationController
   def create
     @vote = current_user.votes.new(opinion_id: params[:opinion_id])
 
-    redirect_to '/' if @vote.save
+    redirect_back fallback_location: '/' if @vote.save
   end
 end
